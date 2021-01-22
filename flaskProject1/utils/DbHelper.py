640fad64efbe23d7c6e708fedbef3ba5ad20b397
sympathyh <sympathyh@foxmail.com>
2021-01-22 16:34:42 +0800

    修改了DbHelper类

--- flaskProject1/utils/DbHelper.py
+++ flaskProject1/utils/DbHelper.py
@@ -1,2 +1,10 @@
 #数据库工具类
 import pymysql
+
+# db_con=pymysql.Connection('localhost','root','123456','pytestdb')
+db_con=pymysql.connect('localhost','root','123456','pytestdb')
+cursor=db_con.cursor()
+cursor.execute("select version()")
+data=cursor.fetchone()
+cursor.close()
+db_con.close()
