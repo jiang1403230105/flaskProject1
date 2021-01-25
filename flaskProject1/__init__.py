d0016a98db22923317df4dcb44e9df9706687711
sympathyh <sympathyh@foxmail.com>
2021-01-25 16:28:02 +0800

    在DbHelper类增加了两个功能函数

--- flaskProject1/__init__.py
+++ flaskProject1/__init__.py
@@ -6,6 +6,6 @@ def create_app():
     app=Flask(__name__)
     app.config.from_object('settings.Config')#加载配置文件
 
-    app.register_blueprint(index_page)
+    app.register_blueprint(index_page)#注册蓝图
     app.register_blueprint(account)#注册蓝图
     return app
--- flaskProject1/utils/DbHelper.py
+++ flaskProject1/utils/DbHelper.py
@@ -2,9 +2,17 @@
 import pymysql
 
 # db_con=pymysql.Connection('localhost','root','123456','pytestdb')
-db_con=pymysql.connect('localhost','root','123456','pytestdb')
+db_con=pymysql.connect(host='localhost',user='root',password='123456',database='pytestdb',charset='utf8')
 cursor=db_con.cursor()
 cursor.execute("select version()")
 data=cursor.fetchone()
 cursor.close()
 db_con.close()
+
+#创建数据连接
+def create_con():
+    pass
+
+#关闭数据库
+def close_con():
+    pass
