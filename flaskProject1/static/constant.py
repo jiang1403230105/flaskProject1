24bb952db59f2d59914b16c97f6fbf3156c65682
sympathyh <sympathyh@foxmail.com>
2021-01-26 16:05:03 +0800

    constant.py添加数据静态变量

--- flaskProject1/static/constant.py
+++ flaskProject1/static/constant.py
@@ -1,1 +1,15 @@
 #常量文件
+
+#数据库链接参数
+LOCAL_HOST='localhost'
+MYSQL_USER='jyj'
+MYSQL_PWD='666666'
+DATABASE='jyj'
+ENCODING='utf8'
+
+#数据库的表名
+DB_CLASS='class'
+DB_STUDENT='student'
+DB_TEACHER='teacher'
+DB_COURSE='course'
+DB_SCORE='score'
